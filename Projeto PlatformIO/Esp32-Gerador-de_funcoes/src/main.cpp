0e385ab83ad214a52b1aa7b42f2eafc285e361d4
Zebio <mateuseusebio@gmail.com>
2021-04-05 21:31:34 -0300

    adição de mais 2 botões touch e terminada a função de leitura dos botões touch

--- Projeto PlatformIO/Esp32-Gerador-de_funcoes/src/main.cpp
+++ Projeto PlatformIO/Esp32-Gerador-de_funcoes/src/main.cpp
@@ -6,10 +6,12 @@
 /*-----------Mapeamento de Hardware---------------*/
 #define dac1    26
 #define dac2    25
-#define touch1  27
-#define touch2  12
+#define t_volta 27
+#define t_enter 12
+#define t_mais  14
+#define t_menos 13
 #define lcd_rs  19
-#define lcd_e   23
+#define lcd_en  23
 #define lcd_d4  18
 #define lcd_d5  17
 #define lcd_d6  16
@@ -17,7 +19,7 @@
 
 
 /*------Inicialização da biblioteca do display----*/
-LiquidCrystal lcd(lcd_rs, lcd_e, lcd_d4, lcd_d5, lcd_d6, lcd_d7);
+LiquidCrystal lcd(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7);
 
 
 /*---------------Variáveis Globais----------------*/
@@ -26,21 +28,28 @@ double  canal1             ,canal2,
         offset1     = 1.65 ,offset2     = 1.65,
         frequencia1 = 0.1  ,frequencia2 = 0.1,
         radianos1   = 0    ,radianos2   = 0;
+bool flag_touch;
+int tempo_touch;
         
       
 /*---------------Constantes Globais----------------*/
-
-long double constante_DAC = 255/3.3;
+const long double constante_DAC = 255/3.3;
+const int tempo_ms=200;
+const int teclado_touch[4]= {t_volta,t_enter,t_mais,t_menos};
 
 
 /*-----------Declaração de Funções---------------*/
-bool le_touch(int);
+int le_touch();
 long double seno(double,double,double,double &);
 long double dente_de_serra(double,double,double,double &);
 
 
 /*------------Configurações Iniciais-------------*/
 void setup() {
+  pinMode(t_enter,INPUT);
+  pinMode(t_volta,INPUT_PULLDOWN);
+  pinMode(t_mais,INPUT_PULLDOWN);
+  pinMode(t_menos,INPUT);
   Serial.begin(115200);
   lcd.begin(16, 2);
   lcd.print("Gerador de Funcoes");
@@ -49,18 +58,39 @@ void setup() {
 
 /*-----------------Loop Infinito------------------*/
 void loop() {
+  /*
   dacWrite(dac1,canal1);
   dacWrite(dac1,canal2);
   canal1=dente_de_serra(offset1,frequencia1,amplitude1,radianos1)*(constante_DAC);
   canal2=seno(offset2,frequencia2,amplitude2,radianos2)*(constante_DAC);
+  
   delay(1);
+  */
+  Serial.println(int(le_touch()));
 }
 
 
 /*-----------Implementação das Funções-------------*/
-bool le_touch(int touch)
-{
-  return (touchRead(touch)<35);
+int le_touch(){
+  if ((millis()-tempo_touch)>=tempo_ms)
+  {
+    for (int botao=0;botao<4;botao++)
+    {
+      if ((touchRead(teclado_touch[botao])<35)&&(touchRead(teclado_touch[botao])<35))
+      {
+        flag_touch=true;
+        tempo_touch=millis();
+        return teclado_touch[botao];
+      }
+    }
+    return false;
+  }
+  else if (flag_touch)
+  {
+    flag_touch=false;
+    tempo_touch=millis();
+  }
+  return false;
 }
 
 long double seno(double offset, double frequencia,double amplitude,double &radianos)
